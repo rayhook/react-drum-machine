43c0c8525e1b0a6cbb85c17feac7086566a61eae
rayhook <koohyar@gmail.com>
2020-02-24 11:52:36 +0100

    Updates

---
+++ precache-manifest.bd73f2b253fd718d1bdb8ef6f2b6002d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5592b915561188257eaa163ee6ab4d0c",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "36f653ef277044e9437d",
+    "url": "/react-drum-machine/static/css/main.7124d5d1.chunk.css"
+  },
+  {
+    "revision": "ef1b443ddd494ee552b2",
+    "url": "/react-drum-machine/static/js/2.117d2b8f.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/react-drum-machine/static/js/2.117d2b8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36f653ef277044e9437d",
+    "url": "/react-drum-machine/static/js/main.42aa79c0.chunk.js"
+  },
+  {
+    "revision": "b545cf50c48665143df9",
+    "url": "/react-drum-machine/static/js/runtime-main.c5ce9622.js"
+  },
+  {
+    "revision": "2a13391023ce8787887331530cac35a7",
+    "url": "/react-drum-machine/static/media/BungeeInline-Regular.2a133910.ttf"
+  }
+]);
